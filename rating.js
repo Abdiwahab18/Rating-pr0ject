ce6a45ca6d39d5bddfebf0e9746cf1600dd6ed3e
abdiwahab <cabdiwahabsalaad@gmail.com>
2024-10-10 20:05:30 +0300

    Ending the project

--- rating.js
+++ rating.js
@@ -1,21 +1,4 @@
-// import { txtElmant } from "./thank-you.js";
-// import { thnkcart } from "./thank-you.js";
-//  export let value1;
-
-let cart=[
-  {
-    logo:'<button class="btn  start-logo"><svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="m9.067.43 1.99 4.031c.112.228.33.386.58.422l4.45.647a.772.772 0 0 1 .427 1.316l-3.22 3.138a.773.773 0 0 0-.222.683l.76 4.431a.772.772 0 0 1-1.12.813l-3.98-2.092a.773.773 0 0 0-.718 0l-3.98 2.092a.772.772 0 0 1-1.119-.813l.76-4.431a.77.77 0 0 0-.222-.683L.233 6.846A.772.772 0 0 1 .661 5.53l4.449-.647a.772.772 0 0 0 .58-.422L7.68.43a.774.774 0 0 1 1.387 0Z" fill="#FC7614"/></svg></button>',
-    question:'how did we do?',
-    text:' Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!',
-    // button:{
-    //  btn1:1,
-    //  btn2:2,
-    //  btn3:3,
-    //  btn4:4,
-    //  btn5:5
-    // },
-  }
-];
+import { cart } from "./carts.js";
 
 let cartHtml='';
 cart.forEach((carts)=>{
@@ -45,7 +28,7 @@ cart.forEach((carts)=>{
         </p>
     
       
-        <a href="rating-thank-you.html" >  <button  class="Submit  js-submit" >Submit</button></a>
+        <a  >  <button  class="Submit  js-submit" >Submit</button></a>
       </div>
       </div>
   
@@ -54,12 +37,20 @@ cart.forEach((carts)=>{
 document.querySelector('.js-main').innerHTML=cartHtml;
 
 
+let myRate;
 
+document.querySelectorAll('.btn')
+.forEach(item=>{
+    item.addEventListener('click',()=>{
+myRate=Number(item.innerHTML);
+console.log(myRate)
+    })
+  })
 
-// document.querySelectorAll('.btn')
-//   .forEach(item=>{
-//     item.addEventListener('click',()=>{
-// value1=Number(item.innerHTML);
-// return value1
-//     })
-//   })
+  
+
+  let sumb=document.querySelector('.js-submit');
+  sumb.addEventListener('click',()=>{
+localStorage.setItem('myRate',myRate);
+location.href="rating-thank-you.html"
+  })
